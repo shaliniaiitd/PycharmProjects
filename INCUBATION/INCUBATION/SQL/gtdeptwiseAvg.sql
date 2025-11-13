23341605996bcce78b83d156d9b73ad92fe3ca20
shaliniaiitd <107319118+shaliniaiitd@users.noreply.github.com>
2025-11-13 18:35:31 +0530

    removed comma

--- INCUBATION/INCUBATION/SQL/gtdeptwiseAvg.sql
+++ INCUBATION/INCUBATION/SQL/gtdeptwiseAvg.sql
@@ -1,5 +1,5 @@
-select e.emp_id from employee e,
+select e.emp_id from employee e
 inner join
-(select Avg(salary) as avg_salary from employee group by department_name) viewSalary
+(select department_name, Avg(salary) as avg_salary from employee group by department_name) viewSalary
 on e.department_name = viewSalary.department_name
 where e.salary > viewSalary.avg_salary
